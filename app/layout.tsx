abd9f260d2f7d4099f71b66aa5f66b64c93b763f
akmmubashir <akmmubashir@gmail.com>
2025-12-21 01:31:32 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,52 @@ const montserrat = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: "Real Estate Broker",
-  description: "Luxury Real Estate Broker - Discover Your Dream Home with Us",
+  metadataBase: new URL("https://real-estate-broker-landing-page.vercel.app"),
+  alternates: {
+    canonical: "https://real-estate-broker-landing-page.vercel.app",
+  },
+  title: "Real Estate Broker | Luxury Properties in Dubai",
+  description: "Real Estate Broker is your trusted partner for luxury properties in Dubai. Discover your dream home with us.",
+  keywords: [
+    "Real Estate Broker", "Luxury Properties", "Dubai Real Estate", "Apartments for Sale", "Villas for Sale", "Property Investment", "Real Estate Agency", "Luxury Homes", "Dubai Properties", "Real Estate Listings",
+  ],
+  authors: [{ name: "Real Estate Broker" }],
+  creator: "Real Estate Broker",
+  publisher: "Real Estate Broker",
+
+
+  openGraph: {
+    title: "Real Estate Broker | Luxury Properties in Dubai",
+    description:
+      "Real Estate Broker is your trusted partner for luxury properties in Dubai. Discover your dream home with us.",
+    url: "https://real-estate-broker-landing-page.vercel.app",
+    siteName: "Real Estate Broker",
+    type: "website",
+    locale: "en_US",
+    images: [
+      {
+        url: "/assets/common/image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Real Estate Broker | Luxury Properties in Dubai",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Real Estate Broker | Luxury Properties in Dubai",
+    description:
+      "Real Estate Broker is your trusted partner for luxury properties in Dubai. Discover your dream home with us.",
+    images: ["/assets/common/image.jpg"],
+
+  },
+
+  robots: {
+    index: false,
+    follow: false
+  }
+
 };
 
 export default function RootLayout({
