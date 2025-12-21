477626b14952cac703731f8480f590a386b7de28
akmmubashir <akmmubashir@gmail.com>
2025-12-21 23:31:52 +0530

    meta changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,22 +12,22 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://real-estate-broker-landing-page.vercel.app",
   },
-  title: "Real Estate Broker | Luxury Properties in Dubai",
-  description: "Real Estate Broker is your trusted partner for luxury properties in Dubai. Discover your dream home with us.",
+  title: "DAMAC Properties | Luxury Properties in Dubai",
+  description: "DAMAC Properties is your trusted partner for luxury properties in Dubai. Discover your dream home with us.",
   keywords: [
-    "Real Estate Broker", "Luxury Properties", "Dubai Real Estate", "Apartments for Sale", "Villas for Sale", "Property Investment", "Real Estate Agency", "Luxury Homes", "Dubai Properties", "Real Estate Listings",
+    "DAMAC Properties", "Luxury Properties", "Dubai Real Estate", "Apartments for Sale", "Villas for Sale", "Property Investment", "Real Estate Agency", "Luxury Homes", "Dubai Properties", "Real Estate Listings",
   ],
-  authors: [{ name: "Real Estate Broker" }],
-  creator: "Real Estate Broker",
-  publisher: "Real Estate Broker",
+  authors: [{ name: "DAMAC Properties" }],
+  creator: "DAMAC Properties",
+  publisher: "DAMAC Properties",
 
 
   openGraph: {
-    title: "Real Estate Broker | Luxury Properties in Dubai",
+    title: "DAMAC Properties | Luxury Properties in Dubai",
     description:
-      "Real Estate Broker is your trusted partner for luxury properties in Dubai. Discover your dream home with us.",
+      "DAMAC is your trusted partner for luxury properties in Dubai. Discover your dream home with us.",
     url: "https://real-estate-broker-landing-page.vercel.app",
-    siteName: "Real Estate Broker",
+    siteName: "DAMAC Properties",
     type: "website",
     locale: "en_US",
     images: [
@@ -35,16 +35,16 @@ export const metadata: Metadata = {
         url: "/assets/common/image.jpg",
         width: 1200,
         height: 630,
-        alt: "Real Estate Broker | Luxury Properties in Dubai",
+        alt: "DAMAC Properties | Luxury Properties in Dubai",
       },
     ],
   },
 
   twitter: {
     card: "summary_large_image",
-    title: "Real Estate Broker | Luxury Properties in Dubai",
+    title: "DAMAC Properties | Luxury Properties in Dubai",
     description:
-      "Real Estate Broker is your trusted partner for luxury properties in Dubai. Discover your dream home with us.",
+      "DAMAC is your trusted partner for luxury properties in Dubai. Discover your dream home with us.",
     images: ["/assets/common/image.jpg"],
 
   },
